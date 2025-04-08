556b80128da8553de3b3b9d9b4ff7dd4343d5bb6
DavitGasparyan12 <davit.a.gasparyan@gmail.com>
2025-04-08 22:15:59 +0400

    Update ex4.py

--- elif/ex4.py
+++ elif/ex4.py
@@ -1,6 +1,7 @@
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 number3 = int(input("Enter third number: "))
+
 if (number1 > number2) and (number1 > number3):
    print(f"largest number is {number1}")
 elif (number2 > number1) and (number2 > number3):
@@ -8,16 +9,16 @@ elif (number2 > number1) and (number2 > number3):
 elif (number3 > number1) and (number3 > number2):
    print(f"largest number is {number3}")
 elif (number1 == number2) and (number1==number2 > number3):
-   print(f"{number1} and {number2} is largest ")
+   print(f"{number1} is largest ")
 elif (number1 == number2) and (number1==number2 < number3):
    print(f"largest number is {number3}")
 elif (number1 == number3) and (number1==number3 > number2):
-   print(f"{number1} and {number3} is largest ")
+   print(f"{number1}  is largest ")
 elif (number1 == number3) and (number1==number3 < number2):
    print(f"largest number is {number2}")
 elif (number2 == number3) and (number2==number3 > number1):
-   print(f"{number2} and {number3} is largest")
+   print(f"{number2} is largest")
 elif (number2 == number3) and (number2==number3 < number1):
    print(f"largest number is {number1}")
 else:
-   print(f"{number1} and {number2} and {number3} are equal")
+   print("all numbers are equal")
